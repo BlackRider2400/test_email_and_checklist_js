d15515052517dab99816bd15eeca995fc0eee64a
BlackRider2400 <krzysztof@baralkiewicz.pl>
2021-06-13 22:21:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  var devices = ["shop", "cinema", "pharmacy"];
+  var emails = ["johnsmith@gmail.com", "johnsmithlegitmail@gmail.com", "test@test.com"];
+  var alerts_container = $('[alert_div_container]');
+  var alertTemplate = $('[alert_field]');
+
+  getAllAlerts();
+
+  function getAllAlerts(){
+
+  }
+});

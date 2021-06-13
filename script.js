7fa4b1d591232b361594d2f8279c47e7f473ff62
BlackRider2400 <krzysztof@baralkiewicz.pl>
2021-06-13 23:58:23 +0200

    base version

--- script.js
+++ script.js
@@ -2,11 +2,28 @@ $(document).ready(function(){
   var devices = ["shop", "cinema", "pharmacy"];
   var emails = ["johnsmith@gmail.com", "johnsmithlegitmail@gmail.com", "test@test.com"];
   var alerts_container = $('[alert_div_container]');
-  var alertTemplate = $('[alert_field]');
+  var alertTemplate = $('[alert_field]').children()[0];
 
   getAllAlerts();
 
   function getAllAlerts(){
+    handleAlertRowRender(emails);
+  }
+  function handleAlertRowRender(data){
+    data.forEach(i => {
+      createAlertRow(i).appendTo(alerts_container);
+    });
 
+  }
+  function createAlertRow(data){
+    var element = $(alertTemplate).clone();
+    var checkbox = element.find('[device_checkbox]').children()[0];
+    element.find('[email_input]').val(data);
+    devices.forEach(i => {
+      var element_checkbox = $(checkbox).clone();
+      element_checkbox.find("[device_name]").text(i);
+      element_checkbox.appendTo(element);
+    });
+    return element;
   }
 });
